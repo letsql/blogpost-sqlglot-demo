20bab1901504d9d9e760de6c51008ec7be8e4613
Daniel Mesejo <mesejoleon@gmail.com>
2024-03-20 12:27:03 +0100

    refactor: change code to fit screen

--- demo.py
+++ demo.py
@@ -10,10 +10,12 @@ engine = create_engine('postgresql://user:pass@localhost:5432/patients')
 query = "SELECT * FROM patients;"
 df = pd.read_sql_query(query, engine)
 
-X = df.set_index('eid').drop(columns=[col for col in df.columns if col.startswith('facid_')])
+columns_to_drop = [col for col in df.columns if col.startswith('facid_')]
+X = df.set_index('eid').drop(columns=columns_to_drop)
 
 predictions = model.predict(X)
 df_with_predictions = X.assign(lengthofstay=predictions)
-filtered = df_with_predictions[df_with_predictions["rcount"].lt(2) & df_with_predictions["lengthofstay"].gt(1)]
+mask = df_with_predictions["rcount"].lt(2) & df_with_predictions["lengthofstay"].gt(1)
+filtered = df_with_predictions[mask]
 result = filtered.reset_index()[['eid']]
 print(result)
